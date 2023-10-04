6e62cbe812cd7a62072ca1d95102a89adea0d35a
Denys <gusardavydov2004@gmail.com>
2023-10-04 15:24:26 +0300

    all tasks are done

--- lab02.js
+++ lab02.js
@@ -15,7 +15,7 @@ const courses = [
     'Statistics',
 ];
 
-function task1(users, additionalUsers) {
+function makeUsers(users, additionalUsers) {
     const newUsers = []
     let id = 0
 
@@ -29,13 +29,14 @@ function task1(users, additionalUsers) {
             state: user.location.state,
             country: user.location.country,
             course: courses[Math.floor(Math.random() * courses.length)],
-            favorite: true,
+            favorite: Math.random() < 0.5,
             postcode: user.location.postcode,
             coordinates: user.location.coordinates,
             timezone: user.location.timezone,
             email: user.email,
             b_date: user.dob.date,
             age: user.dob.age,
+            bg_color: "#" + Math.floor(Math.random() * 256).toString(16) + Math.floor(Math.random() * 256).toString(16) + Math.floor(Math.random() * 256).toString(16),
             phone: user.phone,
             picture_large: user.picture.large,
             picture_thumbnail: user.picture.thumbnail,
@@ -54,12 +55,17 @@ function task1(users, additionalUsers) {
     return newUsers
 }
 
+function isNeededString(str) {
+    return typeof str === 'string' && str.at(0).toUpperCase() === str.at(0);
+}
+
 function validateObject(user) {
-    if(/[A-Z][a-z]*\s[A-Z][a-z]*$/.test(user.full_name) && /[A-Z][a-z]*$/.test(user.state) &&
-        /([A-Z][a-z]*)|([A-Z][a-z]*(\s[A-Z][a-z]*)*)$/.test(user.country) &&
-        /([A-Z][a-z]*)|([A-Z][a-z]*(\s[A-Z][a-z]*)*)$/.test(user.city) &&
+    if((/[A-Z][a-z]*\s[A-Z][a-z]*$/.test(user.full_name) || isNeededString(user.full_name)) &&
+        (/[A-Z][a-z]*$/.test(user.state) || isNeededString(user.state)) &&
+        (/([A-Z][a-z]*)|([A-Z][a-z]*(\s[A-Z][a-z]*)*)$/.test(user.country) || isNeededString(user.country)) &&
+        (/([A-Z][a-z]*)|([A-Z][a-z]*(\s[A-Z][a-z]*)*)$/.test(user.city) || isNeededString(user.city)) &&
         /[a-z]*$/.test(user.gender) &&
-        /([A-Z][a-z]*)|([A-Z][a-z]*(\s[A-Z][a-z]*)*)$/.test(user.note) &&
+        (/([A-Z][a-z]*)|([A-Z][a-z]*(\s[A-Z][a-z]*)*)$/.test(user.note) || isNeededString(user.note)) &&
         /([A-Za-z0-9_-]+\.)*[A-Za-z0-9_-]+@[a-z]+\.[a-z]+$/.test(user.email) &&
         (/\+38\d{10}$/.test(user.phone) || //UA
         /\d{4}-\d{7}$/.test(user.phone) || // DE
@@ -73,20 +79,21 @@ function validateObject(user) {
         /\d{3}\s\d{3}\s\d{2}\s\d{2}$/.test(user.phone) || // CH
         /\d{3}-\d{3}-\d{4}$/.test(user.phone)) && // IE CA
         typeof user.age === 'number') {
-        // console.log(user)
         return true;
     }
 
-    console.log(user)
     return false;
 }
 
 function filtration(users, filtrationKey) {
     let resUsers = []
 
+    const min = filtrationKey.age.split("-")[0]
+    const max = filtrationKey.age.split("-")[1]
+
     users.forEach(user => {
        if((user.country === filtrationKey.country || filtrationKey.country === undefined) &&
-           (user.age === filtrationKey.age || filtrationKey.age === undefined) &&
+           ((user.age >= min && user.age <= max) || filtrationKey.age === undefined) &&
            (user.gender === filtrationKey.gender || filtrationKey.gender === undefined) &&
            (user.favorite === filtrationKey.favorite || filtrationKey.favorite === undefined)){
            resUsers.push(user)
@@ -108,7 +115,51 @@ function search(users, searchKey) {
 
     return users.find(user => ((user.full_name === name || name === undefined) && (user.note === note || note === undefined) && (user.age === Number(age) || age === undefined)))
 }
-// console.log(additionalUsers)
-const a = task1(randomUserMock, additionalUsers);
 
-console.log(search(a, "27"))
+function sort(users, key, flag) {
+    let sortUsers = [...users]
+    switch (key) {
+        case "full_name" :
+            if(flag) {
+                sortUsers.sort((a, b) => a.full_name.localeCompare(b.full_name))
+            } else {
+                sortUsers.sort((a, b) => b.full_name.localeCompare(a.full_name))
+            }
+            break
+        case "age":
+            if(flag) {
+                sortUsers.sort((a, b) => a.age - b.age)
+            } else {
+                sortUsers.sort((a, b) => b.age - a.age)
+            }
+            break
+        case "b_day":
+            if(flag) {
+                sortUsers.sort((a, b) => a.localeCompare(b))
+            } else {
+                sortUsers.sort((a, b) => b.localeCompare(a))
+            }
+            break
+        case "country":
+            if(flag) {
+                sortUsers.sort((a, b) => a.localeCompare(b))
+            } else {
+                sortUsers.sort((a, b) => b.localeCompare(a))
+            }
+            break
+        default:
+            console.log("Wrong sort key")
+    }
+
+    return sortUsers
+}
+
+function countPercents(users, percentKey) {
+    const filterUsers = filtration(users, percentKey)
+
+    return (filterUsers.length / users.length) * 100
+}
+
+// console.log(additionalUsers)
+const a = makeUsers(randomUserMock, additionalUsers);
+console.log(countPercents(a, {age:"18-30"}))
